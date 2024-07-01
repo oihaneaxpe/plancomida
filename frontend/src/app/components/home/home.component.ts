6621f814bb9d1cf8353321f8e448b0cd13242958
oihaneaxpe <43204760+oihaneaxpe@users.noreply.github.com>
2024-07-01 02:41:27 +0200

    Se cambia el diseño de la pagina home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -45,7 +45,62 @@ import { AuthService } from '../../services/auth.service';
 export class HomeComponent {
   title = 'Health Planner';
 
+  homeButtons = [
+    {
+      imgPath: '/assets/images/home/food-plan.jpeg',
+      title: 'Planificación',
+      description: 'Planifica tus menús semanales.',
+      icon: 'event_note',
+      link: '/food-plan',
+      disponible: true,
+      anonimous: false
+    },
+    {
+      imgPath: '/assets/images/home/recipes.jfif',
+      title: 'Recetas',
+      description: 'Descubre nuevas recetas.',
+      icon: 'restaurant_menu',
+      link: '/recipies',
+      disponible: true,
+      anonimous: true
+    },
+    {
+      imgPath: '/assets/images/home/reports.jfif',
+      title: 'Informes',
+      description: 'Analiza tu progreso.',
+      icon: 'bar_chart',
+      link: '/reports',
+      disponible: false,
+      anonimous: false
+    },
+    {
+      imgPath: '/assets/images/home/blog.jfif',
+      title: 'Blog',
+      description: 'Lee nuestros consejos y recomendaciones.',
+      icon: 'book',
+      link: '/blog',
+      disponible: false,
+      anonimous: true
+    },
+    {
+      imgPath: '/assets/images/home/habits.png',
+      title: 'Resumen diario',
+      description: 'Registra tu consumo diario para obtener recomendaciones.',
+      icon: 'assignment',
+      link: '/food-plan',
+      disponible: true,
+      anonimous: false
+    },
+    {
+      imgPath: '/assets/images/home/tips.jfif',
+      title: 'Consejos y trucos',
+      description: 'Lee nuestros consejos y recomendaciones.',
+      icon: 'lightbulb',
+      link: '/food-plan',
+      disponible: true,
+      anonimous: true
+    },
+  ];
   constructor(public authService: AuthService) {
-    // Simula la lógica de inicio de sesión
   }
 }
